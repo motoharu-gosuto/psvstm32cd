70c5654c57a88876712465c94d156b4f67be39b2
motoharu-gosuto <motoharu.gosuto@gmail.com>
2019-05-04 00:00:05 +0400

    move sys clk init to separate files

---
+++ psvstm32cd/Inc/sys_clk.h
@@ -0,0 +1,17 @@
+#ifndef __sys_clk_H
+
+   #define __sys_clk_H
+   
+   #ifdef __cplusplus
+    extern "C" {
+   #endif
+
+   #include "main.h"
+
+   void SystemClock_Config(void);
+
+   #ifdef __cplusplus
+   }
+   #endif
+   
+#endif
