9268746e8a713d23dfb4ac7e69560be3134f82ca
motoharu-gosuto <motoharu.gosuto@gmail.com>
2019-05-04 23:47:36 +0400

    rework main function to call both mmc init and usb init.

--- psvstm32cd/Src/main.c
+++ psvstm32cd/Src/main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include "error_handler.h"
 #include "usb_device.h"
-
 #include "gpio.h"
 #include "sys_clk.h"
+
+#include "game_card.h"
 
 int button_hold_counter = 0;
 
@@ -17,11 +18,37 @@ int main(void)
 
    // initialize gpio
    MX_GPIO_Init();
+   
+   HAL_Delay(100);
+   
+   // somehow initialization of card called from STORAGE_Init_FS does not work - hangs forever
+   
+   // write to blue pin - indicate loading
+   HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
+   
+   MBR mbr;
+
+   if(StartGameCardInit(&mbr) != HAL_OK)
+   {
+      // toggle red pin if game card initialization has failed
+      HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
+   }
+   else
+   {
+      // toggle once green pin if everything is ok
+      HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+      HAL_Delay(500);
+      HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+      HAL_Delay(500);
+   }
+   
+   // write to blue pin - indicate that initialization is over
+   HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
 
    while (1)
    {
       //toggle orange pin (show that device is ready by blinking the led)
-      HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
+      //HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
       
       // read button input
       GPIO_PinState ps = HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin);
@@ -36,15 +63,22 @@ int main(void)
          if(button_hold_counter == 0)
          {
             // initialize usb
-            if(MX_USB_DEVICE_Init() == USBD_OK)
-            {
-               // toggle green pin if everything is ok
-               HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);  
-            }
-            else
+            if(MX_USB_DEVICE_Init() != USBD_OK)
             {
                // toggle red pin if MMC initialization has failed
                HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
+            }
+            else
+            {
+               // toggle twice green pin if everything is ok
+               HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+               HAL_Delay(500);
+               HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+               HAL_Delay(500);
+               HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+               HAL_Delay(500);
+               HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
+               HAL_Delay(500);
             }
             
             button_hold_counter++;
