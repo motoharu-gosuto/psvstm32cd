d3463c66c76717802416e3585b85dc2cc2c25bf8
motoharu-gosuto <motoharu.gosuto@gmail.com>
2019-05-04 23:50:21 +0400

    implement usb device

--- psvstm32cd/Src/usbd_storage_if.c
+++ psvstm32cd/Src/usbd_storage_if.c
@@ -1,10 +1,11 @@
 #include "usbd_storage_if.h"
 
-#include "game_card.h"
+#include <string.h>
+
+#include "mbr_types.h"
+#include "sdio.h"
 
 #define STORAGE_LUN_NBR                  1
-#define STORAGE_BLK_NBR                  0x10000
-#define STORAGE_BLK_SIZ                  0x200
 
 // USB Mass storage Standard Inquiry Data.
 const int8_t STORAGE_Inquirydata_FS[] = {/* 36 */
@@ -47,19 +48,41 @@ USBD_StorageTypeDef USBD_Storage_Interface_fops_FS =
 };
 
 MBR game_card_mbr;
+int gro_partition_index = -1;
 
 // Initializes over USB FS IP
 int8_t STORAGE_Init_FS(uint8_t lun)
 {
-   if(StartGameCardInit(&game_card_mbr) != HAL_OK)
-      return USBD_FAIL;
    return (USBD_OK);
 }
 
 int8_t STORAGE_GetCapacity_FS(uint8_t lun, uint32_t *block_num, uint16_t *block_size)
 {
-   *block_num  = STORAGE_BLK_NBR;
-   *block_size = STORAGE_BLK_SIZ;
+   *block_num  = 0;
+   *block_size = 0;
+   
+   // read sony mbr
+   if(HAL_MMC_ReadBlocks(&hmmc, (uint8_t*)&game_card_mbr, 0, 1, 100) != HAL_OK)
+      return USBD_FAIL;
+   
+   // check header
+   if(memcmp(SCEHeader, game_card_mbr.header, sizeof(game_card_mbr.header)) != 0)
+      return USBD_FAIL;
+   
+   // find gro0 partition
+   for(int i = 0; i < NPartitions; i++)
+   {
+      PartitionEntry* pe = &game_card_mbr.partitions[i];
+      if(pe->partitionCode == gro0)
+      {
+         gro_partition_index = i;
+         break;
+      }
+   }
+   
+   *block_num  = game_card_mbr.partitions[gro_partition_index].partitionSize;
+   *block_size = SD_DEFAULT_SECTOR_SIZE;
+
    return (USBD_OK);
 }
 
@@ -70,17 +93,20 @@ int8_t STORAGE_IsReady_FS(uint8_t lun)
 
 int8_t STORAGE_IsWriteProtected_FS(uint8_t lun)
 {
-   return (USBD_OK);
+   return (USBD_FAIL); // Read Only
 }
 
 int8_t STORAGE_Read_FS(uint8_t lun, uint8_t *buf, uint32_t blk_addr, uint16_t blk_len)
 {
+   if(HAL_MMC_ReadBlocks(&hmmc, buf, blk_addr, blk_len, 10) != HAL_OK)
+      return USBD_FAIL;
+   
    return (USBD_OK);
 }
 
 int8_t STORAGE_Write_FS(uint8_t lun, uint8_t *buf, uint32_t blk_addr, uint16_t blk_len)
 {
-   return (USBD_OK);
+   return (USBD_FAIL); // Read Only
 }
 
 int8_t STORAGE_GetMaxLun_FS(void)
